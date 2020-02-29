6c061bdbaf5cde72c2798125067c0233bc527398
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-29 13:55:31 -0500

    Done.

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ let stack = []
 for(let parenthesis of input) {
 let parenthesisIndex = parentheses.indexOf(parenthesis);
 
-if(parenthesisIndex === -1) {
-  continue;
-}
 
 if(parenthesisIndex % 2 === 0) {
   stack.push(parenthesisIndex + 1); // push next expected brace position
 } else {
-  if(stack.length === 0 || stack.pop() !== parenthesisIndex) {
+  if(stack.pop() !== parenthesisIndex) {
     return false;
   }
 }
